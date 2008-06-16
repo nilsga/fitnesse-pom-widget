befb70a92d81e6bfda36f92481a91f1f40fe3d1c
Torbjørn Vatn (klaras) <torbjorn@vatn.org>
2008-06-16 21:40:43 +0200

    Updated the tests to support the new constructor/static method consept

--- src/test/java/no/bekk/fitnesse/plugins/widgets/PomWidgetTest.java
+++ src/test/java/no/bekk/fitnesse/plugins/widgets/PomWidgetTest.java
@@ -1,5 +1,5 @@
 package no.bekk.fitnesse.plugins.widgets;
-import static org.mockito.Matchers.anyString;
+
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.stub;
 import static org.mockito.Mockito.verify;
@@ -25,7 +25,7 @@ import fitnesse.wikitext.widgets.ParentWidget;
 import fitnesse.wikitext.widgets.TextIgnoringWidgetRoot;
 
 
-public class PowWidgetTest {
+public class PomWidgetTest {
 	
 	@Mock ParentWidget parent;
 	@Mock TextIgnoringWidgetRoot badParent;
@@ -38,77 +38,60 @@ public class PowWidgetTest {
 		initMocks(this);
 	}
 	
-//	@Test
-//	public void shouldSetClassLoaderOnAndStartMavenEmbedder() throws Exception {
-//		PomWidget pomWidget = new PomWidget.Builder(null, "").mavenEmbedder(mavenEmbedder).build();
-//		pomWidget.startMavenEmbedder();
-//		verify(mavenEmbedder).setClassLoader(Thread.currentThread().getContextClassLoader());
-//		verify(mavenEmbedder).start();
-//		assertTrue(pomWidget.embedderStarted);
-//	}
-//	
-//	@Test
-//	public void shouldStopMavenEmbedder() throws Exception {
-//		PomWidget pomWidget = new PomWidget.Builder(null, "").mavenEmbedder(mavenEmbedder).build();
-//		pomWidget.startMavenEmbedder();
-//		pomWidget.stopMavenEmbedder();
-//		verify(mavenEmbedder).stop();
-//	}
-//	
-//	@Test
-//	public void shouldReadMavenProjectFromPom() throws Exception {
-//		String pom = "pom.xml";
-//		stub(mavenEmbedder.readProjectWithDependencies(new File(pom))).toReturn(mavenProject);
-//		
-//		PomWidget pomWidget = new PomWidget.Builder(null, "!pom pom.xml").mavenEmbedder(mavenEmbedder).build();
-//		pomWidget.readMavenProjectFromPom();
-//		
-//		verify(mavenEmbedder).readProjectWithDependencies(new File(pom));
-//		assertTrue(pomWidget.mavenProject != null);
-//	}
-//	
-//	@Test
-//	public void shouldAddOututDirsToClasspathArray() throws Exception {
-//		List<String> classpaths = new ArrayList<String>();
-//		stub(mavenProject.getBuild()).toReturn(build);
-//		stub(build.getOutputDirectory()).toReturn("output");
-//		stub(build.getTestOutputDirectory()).toReturn("testOutput");
-//		
-//		PomWidget pomWidget = new PomWidget.Builder(null, "").mavenProject(mavenProject).classpaths(classpaths).build();
-//		pomWidget.findOutputDirs();
-//	
-//		assertEquals(classpaths.size(), 2);
-//	}
-//
-//	@Test
-//	public void shouldAddArtifactsToClasspathArray() throws Exception {
-//		List<String> classpaths = new ArrayList<String>();
-//		Set<Artifact> artifacts = new HashSet<Artifact>();
-//		Artifact artifact = mock(Artifact.class);
-//		Artifact artifact2 = mock(Artifact.class);
-//		artifacts.add(artifact);
-//		artifacts.add(artifact2);
-//		
-//		stub(mavenProject.getArtifacts()).toReturn(artifacts);
-//		stub(artifact.getFile()).toReturn(new File("path"));
-//		stub(artifact2.getFile()).toReturn(new File("path2"));
-//		stub(artifact.getGroupId()).toReturn("org.fitnesse");
-//		stub(artifact2.getGroupId()).toReturn("org.fitnesse");
-//		stub(parent.getVariable(anyString())).toReturn("m2repo");
-//		
-//		PomWidget pomWidget = new PomWidget.Builder(parent, "").mavenProject(mavenProject).classpaths(classpaths).build();
-//		pomWidget.findArtifacts();
-//		
-//		assertEquals(classpaths.size(), 2);
-//	}
-//	
-//	@Test(enabled=false)
-//	public void shouldCreateClasspathWidgetsForAllElemetsInArray() throws Exception {
-//		List<String> classpaths = new ArrayList<String>();
-//		classpaths.add("classpath");
-//		PomWidget pomWidget = new PomWidget.Builder(parent, "").classpaths(classpaths).build();
-//		pomWidget.createClasspathWidgets();
-//		verify(parent).addChild(pomWidget);
-//		assertEquals(pomWidget.numberOfChildren(), 2);
-//	}
+	@Test
+	public void shouldSetClassLoaderOnAndStartMavenEmbedder() throws Exception {
+		boolean embedderStarted = PomWidget.startMavenEmbedder(mavenEmbedder);
+		verify(mavenEmbedder).setClassLoader(Thread.currentThread().getContextClassLoader());
+		verify(mavenEmbedder).start();
+		assertTrue(embedderStarted);
+	}
+	
+	@Test
+	public void shouldStopMavenEmbedder() throws Exception {
+		PomWidget.startMavenEmbedder(mavenEmbedder);
+		PomWidget.stopMavenEmbedder(mavenEmbedder, true);
+		verify(mavenEmbedder).stop();
+	}
+	
+	@Test
+	public void shouldReadMavenProjectFromPom() throws Exception {
+		String pom = "pom.xml";
+		stub(mavenEmbedder.readProjectWithDependencies(new File(pom))).toReturn(mavenProject);
+		
+		PomWidget.readMavenProjectFromPom(mavenEmbedder, pom);
+		
+		verify(mavenEmbedder).readProjectWithDependencies(new File(pom));
+	}
+	
+	@Test
+	public void shouldAddOututDirsToClasspathArray() throws Exception {
+		List<String> classpaths = new ArrayList<String>();
+		stub(mavenProject.getBuild()).toReturn(build);
+		stub(build.getOutputDirectory()).toReturn("output");
+		stub(build.getTestOutputDirectory()).toReturn("testOutput");
+		
+		PomWidget.findOutputDirs(mavenProject, classpaths);
+	
+		assertEquals(classpaths.size(), 2);
+	}
+
+	@Test
+	public void shouldAddArtifactsToClasspathArray() throws Exception {
+		List<String> classpaths = new ArrayList<String>();
+		Set<Artifact> artifacts = new HashSet<Artifact>();
+		Artifact artifact = mock(Artifact.class);
+		Artifact artifact2 = mock(Artifact.class);
+		artifacts.add(artifact);
+		artifacts.add(artifact2);
+		
+		stub(mavenProject.getArtifacts()).toReturn(artifacts);
+		stub(artifact.getFile()).toReturn(new File("path"));
+		stub(artifact2.getFile()).toReturn(new File("path2"));
+		stub(artifact.getGroupId()).toReturn("org.fitnesse");
+		stub(artifact2.getGroupId()).toReturn("org.fitnesse");
+		
+		PomWidget.findArtifacts(mavenProject, classpaths, "m2repo");
+		
+		assertEquals(classpaths.size(), 2);
+	}
 }
